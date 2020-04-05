933bf54feeff687002aa82a90222dd25f4e82a43
Jon Chan <me@jonhmchan.com>
2020-04-05 15:18:31 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+# Do not change anything in this file for this exercise
+import os
+import api
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.register_blueprint(api.pokemon, url_prefix="/api")
+app.register_blueprint(api.teams, url_prefix="/api")
+
+# Home page route that serves index.html
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Detail page route that serves detail.html
+# For example /1 will give you the detail page for Bulbasaur
+@app.route('/pokemon/<int:id>')
+def detail_id(id):
+    return render_template('pokemon/detail.html')
+
+# Teams detail page route that serves teams/detail.html
+# For example /1 will give you the detail page for Ash's Team
+@app.route('/teams/<int:id>')
+def teams_id(id):
+    return render_template('teams/detail.html')
+
+# Teams edit page route that serves teams/edit.html
+# For example /1 will let you edit Ash's Team
+@app.route('/teams/<int:id>/edit')
+def teams_id_edit(id):
+    return render_template('teams/edit.html')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
